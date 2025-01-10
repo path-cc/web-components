8f89e4dce38e0f52dcb634688565fbe05ba2f0e9
Cannon Lock <clock@wisc.edu>
2025-01-10 16:08:35 -0600

    Add filterArticlees

--- src/utils/getArticles.ts
+++ src/utils/getArticles.ts
@@ -1,40 +1,9 @@
-import matter from "gray-matter"
-import {BackendArticle, website, tag, Image, article_type} from "../types";
-
-function getSlug(path: string){
-	const splitSlug = path.slice(0, -3).split("-")
-	return [splitSlug[0], splitSlug[1], splitSlug[2], splitSlug.slice(3).join("-")]
-}
-
-function getDate(path: string){
-	const splitSlug = path.slice(0, -3).split("-")
-	return new Date(Date.parse(splitSlug.slice(0, 3).join("-")))
-}
+import {BackendArticle} from "../types";
+import getArticle from "./getArticle";
 
 function isArticle(path: string){
 	const regex = /\d\d\d\d-\d\d?-\d\d?-.*?\.md/g
 	return path.search(regex) != -1
-}
-
-export async function getArticle(organization: string, repo: string, path: string, branch: string): Promise<BackendArticle> {
-	const text = await getRawFile(organization, repo, path, branch)
-	const frontMatter = matter(text)
-
-	return {
-		path: path,
-		slug: getSlug(path),
-		date: getDate(path),
-		content: frontMatter.content,
-		title: frontMatter.data?.title as string,
-		author: frontMatter.data?.author as string,
-		publish_on: frontMatter.data?.publish_on as website[],
-		type: frontMatter.data?.type as "news" | "user",
-		tag: frontMatter.data?.tag as tag,
-		image: frontMatter.data?.image as Image,
-		excerpt: frontMatter.data?.excerpt as string,
-		banner_src: frontMatter.data?.banner_src as string,
-		banner_alt: frontMatter.data?.banner_alt as string
-	}
 }
 
 export async function getArticles(organization: string, repo: string, branch: string): Promise<BackendArticle[]> {
