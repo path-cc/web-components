5c124460ddafc304053ce87508d1fd62d02d58d0
fallow64 <fallow640@gmail.com>
2025-03-04 14:31:23 -0600

    Generalize Staff type

--- stories/mockData/StaffCard.ts
+++ stories/mockData/StaffCard.ts
@@ -5,8 +5,6 @@ export const defaultStaff: Staff = {
   image: "https://placehold.co/150x150",
   title: "Research Software Engineer",
   institution: "University of Wisconsin—Madison",
-  status: "Staff",
-  organizations: ["chtc", "path"],
 };
 
 export const defaultLeader: Staff = {
@@ -14,7 +12,4 @@ export const defaultLeader: Staff = {
   image: "https://placehold.co/150x150",
   title: "Principal Investigator",
   institution: "University of Wisconsin—Madison",
-  status: "Staff",
-  promoted: true,
-  organizations: ["chtc", "path"],
 };
