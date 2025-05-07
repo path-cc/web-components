0100b9110f53c5239b783f09b6b907472b6de4e0
fallow64 <fallow640@gmail.com>
2025-05-07 09:28:16 -0500

    Initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    plugins: [new TsconfigPathsPlugin({ configFile: "src/tsconfig.json" })],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    globalObject: "this",
+    library: {
+      name: "CHTCWebComponents",
+      type: "umd",
+    },
+  },
+  externals: {
+    react: "React", // Case matters here
+    "react-dom": "ReactDOM", // Case matters here
+  },
+};
